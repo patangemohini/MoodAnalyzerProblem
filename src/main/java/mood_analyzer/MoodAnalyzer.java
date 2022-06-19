5cb1112acac4a7a6934789f6820a5abf096e5407
mohini patange <patangemohini5@gmail.com>
2022-06-19 21:10:09 +0530

    Added : Welcome message

---
+++ src/main/java/mood_analyzer/MoodAnalyzer.java
@@ -0,0 +1,7 @@
+package mood_analyzer;
+
+public class MoodAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood analyzer program!!");
+    }
+}
---
+++ src/test/java/mood_analyzer/MoodAnalyzerTest.java
@@ -0,0 +1,10 @@
+package mood_analyzer;
+
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad(){
+
+    }
+}
